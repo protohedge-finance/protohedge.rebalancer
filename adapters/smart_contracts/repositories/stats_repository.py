597a8a0d29b8db48e22f1c9a3d47c3a577701187
Rafael Goesmann <rgo51@uclive.ac.nz>
2022-12-31 18:18:49 +1300

    Save rebalance history using zadd

--- adapters/smart_contracts/repositories/stats_repository.py
+++ adapters/smart_contracts/repositories/stats_repository.py
@@ -1,9 +1,13 @@
+import json
 from redis import Redis 
+from datetime import datetime
 
 class StatsRepository:
 	def __init__(self, redis: Redis):
 		self.redis: Redis = redis	
 	
-	def add_rebalance_outcome(self, outcome: str):
-		self.redis.lpush("rebalance_outcomes", outcome)
+	def add_rebalance_history(self, address: str, note: str):
+		timestamp = int(datetime.utcnow().timestamp()*1e3)
+		record = "{}:{}:{}" .format(address, timestamp, note)
+		self.redis.zadd("rebalance_history", { record: 0 })
 		
